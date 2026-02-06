7aa0ce729c5f50363e552aac32b1c8c81f50ea75
David <davidvalencia0526@gmail.com>
2026-02-05 22:54:11 -0500

    refactor: separate of concerns

---
+++ script.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                oriaBlack: '#0a0a0a',      // Fondo casi negro
+                oriaDark: '#121212',       // Fondo secundario
+                oriaGray: '#1f1f1f',       // Tarjetas
+                oriaNeon: '#DFFF00',       // Amarillo Neón (Ajustado a tu logo)
+                oriaText: '#e5e5e5',       // Blanco suave para texto
+            },
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            }
+        }
+    }
+}
